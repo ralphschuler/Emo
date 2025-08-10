54d38694b6db8e42334abac95af32023cc328573
Ralph Schuler <ralph.schuler@nyphon.de>
2025-08-10 16:12:23 +0200

    Update index.ts

--- packages/st7789-driver/src/index.ts
+++ packages/st7789-driver/src/index.ts
@@ -187,7 +187,7 @@ if (isDirect) {
       device: "/dev/spidev0.0",
       mode: 0,
       bits: 8,
-      speedHz: 32_000_000,
+      speedHz: 64_000_000,
       gpioChip: "gpiochip0",
       dcPin: 25,
       resetPin: 27,
